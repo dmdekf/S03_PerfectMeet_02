730f5e327e9591075f8158ee59722897f6fa28c4
고영욱 <gyw8526@naver.com>
2020-09-14 17:10:20 +0900

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <app-navbar></app-navbar>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import AppNavbar from "@/views/partials/Navbar.vue"
+import AppFooter from '@/views/partials/Footer.vue'
+export default {
+  name:'app',
+  components:{
+    AppNavbar,
+    AppFooter
+  }
+}
+</script>
+
