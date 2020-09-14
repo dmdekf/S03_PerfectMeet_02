8ba8a38265ff87ad6ce678a35b5a098ae5932541
lshmn951 <tjd951@naver.com>
2020-09-14 16:18:13 +0900

    Add basic backend
    Former-commit-id: 67e7fb5cb12576fbc49a5b086a5c94c519a8d307

---
+++ backend/MatNam/src/main/java/com/POM/MatNam/MatNamApplication.java
@@ -0,0 +1,13 @@
+package com.POM.MatNam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatNamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatNamApplication.class, args);
+	}
+
+}
