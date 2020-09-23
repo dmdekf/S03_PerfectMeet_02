6d503d282b1f222f2ae4e48772c8f1ca4569cf69
나윤지 <dmdekf@gmail.com>
2020-09-23 10:21:19 +0900

    Edit login, join page

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -14,7 +14,7 @@
                         <v-card class="px-4">
                             <v-card-text>
                                 <v-form ref="loginForm" v-model="valid" lazy-validation>
-                                    <v-row>
+                                    <v-row max-width="500px">
                                         <v-col cols="12">
                                             <v-text-field v-model="loginEmail" :rules="loginEmailRules" label="E-mail" required></v-text-field>
                                         </v-col>
@@ -23,12 +23,13 @@
                                         </v-col>
                                         <v-col class="d-flex" cols="12" sm="6" xsm="12">
                                         </v-col>
-                                        <v-spacer></v-spacer>
-                                        <v-col class="d-flex" cols="12" sm="3" xsm="12" align-end>
-                                            <v-btn x-large block :disabled="!valid" color="success" @click="validate"> Login </v-btn>
-                                        </v-col>
                                     </v-row>
                                 </v-form>
+																<v-card-actions>
+																	<v-spacer></v-spacer>
+																	<v-btn color="red"  @click="!dialog" to="/">Close</v-btn>
+																	<v-btn :disabled="!valid" color="success" @click="validate"> Login </v-btn>
+																</v-card-actions>
                             </v-card-text>
                         </v-card>
                     </v-tab-item>
@@ -37,14 +38,11 @@
                             <v-card-text>
                                 <v-form ref="registerForm" v-model="valid" lazy-validation>
                                     <v-row>
-                                        <v-col cols="12" sm="6" md="6">
-                                            <v-text-field v-model="firstName" :rules="[rules.required]" label="First Name" maxlength="20" required></v-text-field>
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="6">
-                                            <v-text-field v-model="lastName" :rules="[rules.required]" label="Last Name" maxlength="20" required></v-text-field>
-                                        </v-col>
                                         <v-col cols="12">
                                             <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="6">
+                                            <v-text-field v-model="NickName" :rules="[rules.required]" label="Nick Name" maxlength="20" required></v-text-field>
                                         </v-col>
                                         <v-col cols="12">
                                             <v-text-field v-model="password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 8 characters" counter @click:append="show1 = !show1"></v-text-field>
@@ -52,12 +50,31 @@
                                         <v-col cols="12">
                                             <v-text-field block v-model="verify" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, passwordMatch]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Confirm Password" counter @click:append="show1 = !show1"></v-text-field>
                                         </v-col>
-                                        <v-spacer></v-spacer>
-                                        <v-col class="d-flex ml-auto" cols="12" sm="3" xsm="12">
-                                            <v-btn x-large block :disabled="!valid" color="success" @click="validate">Register</v-btn>
-                                        </v-col>
+																				<v-col cols="12" align="center" justify="center">
+                                        <input
+																					id = "file-selector"
+																					ref="file"
+																					type="file"
+																					@change="handleFileUpload()"
+																					/>
+																					<v-btn @click="uploadFile" color="primary">프로필 사진 업로드</v-btn>
+																				</v-col>
+																			
+																					<v-container fluid :v-show="profileUrl" max-height="400">
+																					<v-row justify="center" align="center" max-height="400">
+																					<v-col cols="8" aspect-ratio="2" contain  align="center" justify="center" max-height="400">
+																					<v-img  v-if="profileUrl" :src="url" aspect-ratio="2" max-width="200" max-height="400">
+																					</v-img>
+																					</v-col>
+																					</v-row>
+																					</v-container>
                                     </v-row>
                                 </v-form>
+																<v-card-actions>
+																	<v-spacer></v-spacer>
+																	<v-btn color="red"  @click="!dialog" to="/">Close</v-btn>
+																	<v-btn :disabled="!valid" color="success" @click="validate">Register</v-btn>
+																</v-card-actions>
                             </v-card-text>
                         </v-card>
                     </v-tab-item>
@@ -96,9 +113,7 @@ export default {
         {name:"Register", icon:"mdi-account-outline"}
     ],
     valid: true,
-    
-    firstName: "",
-    lastName: "",
+    NickName: "",
     email: "",
     password: "",
     verify: "",
