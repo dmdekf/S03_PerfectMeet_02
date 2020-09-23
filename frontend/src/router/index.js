9457c790248528c481be018f2352e396e8d43e72
gyw8526 <gyw8526@naver.com>
2020-09-23 14:34:37 +0900

    selectUserInfo

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,20 +1,20 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Vuex from 'vuex'
-import BootstrapVue from 'bootstrap-vue'
-import Main from "../components/Main.vue"
-import selectMap from "../components/SelectMap.vue"
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Vuex from "vuex";
+import BootstrapVue from "bootstrap-vue";
+import Main from "../components/Main.vue";
+import selectMap from "../components/SelectMap.vue";
+import selectUserInfo from "../components/SelectUserInfo.vue";
+import Login from "../components/Login.vue";
 
-import Login from "../components/Login.vue"
-
-Vue.use(BootstrapVue)
-Vue.use(Vuex)
-Vue.use(VueRouter)
+Vue.use(BootstrapVue);
+Vue.use(Vuex);
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path:'/user/login',
-    name: 'userLogin',
+    path: "/user/login",
+    name: "userLogin",
     component: Login,
   },
   {
@@ -23,16 +23,21 @@ const routes = [
     component: Main,
   },
   {
-    path:"/user/selectMap",
+    path: "/user/selectMap",
     name: "SELECTMAP",
     component: selectMap,
-  }
-]
+  },
+  {
+    path: "/user/selectUserInfo",
+    name: "SELECTUSERINFO",
+    component: selectUserInfo,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
 export default router;
