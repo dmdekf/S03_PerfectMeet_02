6119c8264ac6c3c71bc7836103b53630425786c9
lshmn951 <tjd951@naver.com>
2020-09-24 09:49:24 +0900

    S03P23A507-38 비밀번호 찾기 구현

--- backend/MatNam/src/main/java/com/POM/MatNam/user/service/MailSendService.java
+++ backend/MatNam/src/main/java/com/POM/MatNam/user/service/MailSendService.java
@@ -84,4 +84,21 @@ public class MailSendService {
 
         Transport.send(message);
     }
+    
+    public void mailSendWithPassword(String email, String nickname, String password)
+            throws AddressException, MessagingException {
+        Session session = getSetting();
+        String setfrom = MyEmail;
+        String htmlStr = "<h2>안녕하세요 EZI LOG 입니다!</h2><br><br>" + "<h3>" + nickname + "님</h3>" + "현재 고객님의 비밀번호는 "
+                + password + "입니다.";
+
+        MimeMessage message = new MimeMessage(session);
+
+        message.setFrom(new InternetAddress(setfrom));
+        message.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
+        message.setSubject("EZI LOG 비밀 번호 찾기 메일입니다.");
+        message.setText(htmlStr, "utf-8", "html");
+
+        Transport.send(message);
+    }
 }
