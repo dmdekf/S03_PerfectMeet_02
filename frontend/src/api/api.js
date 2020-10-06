65724d09987cffe1213720148fa9af35ac29650b
고영욱 <gyw8526@naver.com>
2020-10-06 11:29:10 +0900

    1006

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 export default {
-  // URL: "http://i3a501.p.ssafy.io:8080",
-  URL: "http://localhost:8080",
+  URL: "http://j3a507.p.ssafy.io/",
+  //URL: "http://localhost:8080",
   ROUTES: {
     searchpost: "/search/",
   },
